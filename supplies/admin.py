4d1b08b736b3506e194a6a3cd478286a0eff1984
Eslam <Eslam.plutaki@gmail.com>
2025-02-22 19:02:22 +0200

    we ready

--- supplies/admin.py
+++ supplies/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import TShirt
+from .models import TShirt, Size
 
-# Register your models here.
-@admin.register(TShirt)
+class TShirtAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'code',)
+    list_filter = ('size',)
+    search_fields = ('name',)
+    filter_horizontal = ('size',)  # عشان يظهر الشيك بوكس للمقاسات
 
-class SuppliesAdmin(admin.ModelAdmin):
-    supp = ('name', 'price', 'code', )
-
-
+admin.site.register(TShirt, TShirtAdmin)
+admin.site.register(Size)
--- supplies/models.py
+++ supplies/models.py
@@ -1,25 +1,24 @@
 import random
 from django.db import models
 
-# Create your models here.
-
-
-
-class TShirt(models.Model):
-    Size_CHOICES = [
+class Size(models.Model):
+    SIZE_CHOICES = [
+        ('S', 'S'),
         ('M', 'M'),
         ('L', 'L'),
         ('XL', 'XL'),
-        ('XXL', 'XXL'),
-        
     ]
+    name = models.CharField(max_length=2, choices=SIZE_CHOICES, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class TShirt(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     imge = models.ImageField(upload_to='supplies/')
-    size = models.CharField(max_length=4, choices=Size_CHOICES)
+    size = models.ManyToManyField(Size)
     code = models.CharField(max_length=4, unique=True, editable=False)
-
-    
 
     def save(self, *args, **kwargs):
         if not self.code:
@@ -28,12 +27,9 @@ class TShirt(models.Model):
 
     def generate_unique_code(self):
         while True:
-            code = str(random.randint(1000, 9999))  # يولد رقم من 4 أرقام
+            code = str(random.randint(0, 9999)).zfill(4)  # كود عشوائي من 4 أرقام أو أقل
             if not TShirt.objects.filter(code=code).exists():
                 return code
 
     def __str__(self):
-        return f"{self.name} - {self.code}"
-
-    def __str__(self):
-        return f"{self.name} - {self.get_size_display()}"
+        return self.name
