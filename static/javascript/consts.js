56baba177dd924076c6437ac8941d3e6774e7653
Eslam <Eslam.plutaki@gmail.com>
2025-03-05 18:15:20 +0200

    some erorr

---
+++ static/javascript/consts.js
@@ -0,0 +1,80 @@
+const THEMES = {
+  magna: {
+    background: "magna.png",
+    color: "#FFFFFF",
+    logo: "images/vozer_white.png",
+    extra: "#FFFFFF"
+  },
+  gold: {
+    background: "gold.png",
+    color: "#46390C",
+    logo: "images/vozer_black.png",
+    extra: "#FFFFFF"
+  },
+  titan: {
+    background: "titan.png",
+    color: "#fce398",
+    logo: "images/vozer_white.png",
+    extra: "#fce398"
+  },
+  flat: {
+    background: "flat.png",
+    color: "#50442B",
+    logo: "images/vozer_black.png",
+    extra: "#FFFFFF"
+  },
+  hero: {
+    background: "hero.png",
+    color: "#FFFFFF",
+    logo: "images/vozer_white.png",
+    extra: "#FFFFFF"
+  },
+  evo: {
+    background: "evo.png",
+    color: "#FFFFFF",
+    logo: "images/vozer_white.png",
+    extra: "#FFFFFF"
+  },
+  black: {
+    background: "black.png",
+    color: "#FFFFFF",
+    logo: "images/vozer_white.png",
+    extra: "#FFFFFF"
+  },
+  orange: {
+    background: "orange.png",
+    color: "#fce398",
+    logo: "images/vozer_white.png",
+    extra: "#fce398"
+  },
+  fighter: {
+    background: "fighter.png",
+    color: "#fce398",
+    logo: "images/vozer_black.png",
+    extra: "#fce398"
+  },
+  purple: {
+    background: "purple.png",
+    color: "#FFFFFF",
+    logo: "images/vozer_white.png",
+    extra: "#FFFFFF"
+  },
+  blue: {
+    background: "blue.png",
+    color: "#000000",
+    logo: "images/vozer_white.png",
+    extra: "#000000"
+  },
+  champ: {
+    background: "champ.png",
+    color: "#05F1FF",
+    logo: "images/vozer_white.png",
+    extra: "#05F1FF"
+  },
+  yellow: {
+    background: "yellow.png",
+    color: "#FFFFFF",
+    logo: "images/vozer_white.png",
+    extra: "#FFFFFF"
+  },
+};
